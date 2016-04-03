2da23776a8fe762d393ec63487860b50cf6136f0
ericlundmark <erilu506@gmail.com>
2016-04-03 21:40:05 +0200

    Added zebra and monarch formatter to support content negotiation

---
+++ LabelConverter/Formatters/MonarchFormatter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using LabelsMain.Models;
+using LabelsMain.Printers;
+
+namespace LabelConverter.Formatters
+{
+    public class MonarchFormatter : MediaTypeFormatter
+    {
+        public MonarchFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/mchp"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return type == typeof (Label);
+        }
+
+        public override async Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content,
+            TransportContext transportContext)
+        {
+            var label = value as Label;
+            var printer = new MonarchPrinter();
+            label.Print(printer);
+            var printedLabel = printer.ToString();
+            await writeStream.WriteAsync(Encoding.UTF8.GetBytes(printedLabel), 0, printedLabel.Length);
+        }
+    }
+}
---
+++ LabelConverter/Formatters/ZebraFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using LabelsMain.Models;
+using LabelsMain.Printers;
+
+namespace LabelConverter.Formatters
+{
+    public class ZebraFormatter : MediaTypeFormatter
+    {
+        public ZebraFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/zpl"));
+        }
+        public override bool CanReadType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return type == typeof(Label);
+        }
+
+        public override async Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content,
+            TransportContext transportContext)
+        {
+            var label = value as Label;
+            var printer = new ZebraPrinter();
+            label.Print(printer);
+            var printedLabel = printer.ToString();
+            await writeStream.WriteAsync(Encoding.UTF8.GetBytes(printedLabel), 0, printedLabel.Length);
+        }
+    }
+}
